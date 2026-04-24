626436a6f9751674324c77ade6f1c7313bcad3d5
universe7creator <universe7creator@users.noreply.github.com>
2026-04-24 05:15:46 +0300

    Deploy: TOML Parser Pro

--- api/process.js
+++ api/process.js
@@ -1,6 +1,6 @@
 const TOML = require('@iarna/toml');
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-License-Key');
